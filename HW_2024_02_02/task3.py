e487cd498441a5037a6fdfb71d40b8d253d2312b
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-02-09 00:39:57 +0400

    Второе домашнее задание

---
+++ HW_2024_02_02/task3.py
@@ -0,0 +1,12 @@
+number_of_minutes= int(input("Введите количество минут: "))
+Counting_the_hours = number_of_minutes// 60 #считаем часы
+Counting_the_minutes = number_of_minutes % 60 #считаем оставшиеся минуты
+print(f"{number_of_minutes} мин-это {Counting_the_hours} час {Counting_the_minutes} мин ")
+
+# Внимательно читайте задание целиком, нужно было так же скопировать пример ввода и пример вывода, тем не менее в первый раз засчитываю. =)
+
+# Пример ввода:
+#     150
+
+# Пример вывода:
+#     150 мин - это 2 час 30 мин
