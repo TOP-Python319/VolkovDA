fa1ba78a30ac16da59f698764c30b2bdd3d6f576
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-02-08 23:41:29 +0400

    ДЗ принято и защитано 12/12

--- task1.py
+++
@@ -1,4 +0,0 @@
-user_name=input('Введите ваше имя:')
-second_name=input('Введите фамилию:')
-user_age=int(input('Введите ваш год рождения:'))
-print(second_name,user_name, ",",2024-int(user_age),)   
